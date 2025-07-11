bf68e3052742d59d66cef0a65564e5653f4f8b8f
dnvishnu <vishnu.dn@dataknobs.com>
2025-07-11 14:07:12 +0530

    Updated config

--- src/config/gcpConfig.js
+++ src/config/gcpConfig.js
@@ -1,15 +1,9 @@
 const { Storage } = require("@google-cloud/storage");
 
-let storage;
+const serviceAccountJSON = JSON.parse(
+  Buffer.from(process.env.GCP_CMS_BASE64, "base64").toString("utf-8")
+);
 
-if (process.env.GCP_CMS_SERVICE_ACCOUNT_BASE64) {
-  const serviceAccountJSON = JSON.parse(
-    Buffer.from(process.env.GCP_CMS_BASE64, "base64").toString("utf-8")
-  );
-
-  storage = new Storage({ credentials: serviceAccountJSON });
-} else {
-  storage = new Storage(); // fallback to default creds
-}
+const storage = new Storage({ credentials: serviceAccountJSON });
 
 module.exports = { storage };
